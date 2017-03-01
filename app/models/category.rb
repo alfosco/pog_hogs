337c0a337aaf96e2ab23e966c6d1034787a1705d
Alex Fosco <alex.hf88@gmail.com>
2017-03-01 15:54:40 -0700

    fixes slug bug

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,15 +2,7 @@ class Category < ApplicationRecord
   has_many :items
   validates_uniqueness_of :title
 
-  def to_param
-    title.downcase
-  end
+  extend FriendlyId
+  friendly_id :title, use: :slugged
 
-  def self.find_by_param(input)
-    find_by_title(input.capitalize)
-  end
-
-  def self.find(input)
-    input.to_i == 0 ? find_by_title(input) : super
-  end
 end
